9c47ed291c954246c7850240bebb31125ea9e7a0
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-10-08 19:54:02 -0300

    Criação do exemplo

--- BadgeExample/ContentView.swift
+++ BadgeExample/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var hasNotification = true
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(#colorLiteral(red: 0.9887183309, green: 0.8061560988, blue: 0.6014263034, alpha: 1)).ignoresSafeArea()
+            
+            Button(action: {}, label: {
+                ZStack(alignment: .topTrailing) {
+                    Image(systemName: "bell")
+                        .font(.system(size: 48))
+                        .foregroundColor(Color(#colorLiteral(red: 0.9975888133, green: 0.5709193349, blue: 0.3004384339, alpha: 1)))
+                        .padding()
+                        .clipShape(Circle())
+                    
+                    if hasNotification {
+                        Text("3")
+                            .font(.system(size: 18, weight: .bold))
+                            .foregroundColor(.white)
+                            .padding(6)
+                            .background(Color.red)
+                            .clipShape(Circle())
+                            .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: 0)
+                            .offset(x: -12)
+                    }
+                }
+            })
+        }
     }
 }
 
---
+++ README.md
@@ -0,0 +1,29 @@
+# Badge de Notificação
+Exemplo de badge de notificação usando SwiftUI. 
+
+## Screenshots
+<p align="center">
+    <img src="https://user-images.githubusercontent.com/16376748/95521423-dbfa5c00-099f-11eb-99c5-58b7ed8b7b1f.png" width="400">&nbsp;
+</p>
+
+## Requisitos
+- iOS 14
+- SwiftUI
+- Xcode 12
+
+## Meta
+- Github: [https://github.com/rphlfc](https://github.com/rphlfc)
+- Instagram: [https://www.instagram.com/rphlfc.dev](https://www.instagram.com/rphlfc.dev)
+- LinkedIn: [http://www.linkedin.com/in/rphlfc](http://www.linkedin.com/in/rphlfc)
+- Twitter: [https://twitter.com/rphlfc](https://twitter.com/rphlfc)
+- Website: [https://rphlfc.github.io](https://rphlfc.github.io)
+- Youtube: [https://www.youtube.com/channel/UC7UXnCqiqsV4R9lCwKh-rwg](https://www.youtube.com/channel/UC7UXnCqiqsV4R9lCwKh-rwg)
+
+
+
+
+
+
+
+
+
