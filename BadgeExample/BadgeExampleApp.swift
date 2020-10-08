ee753885f9c71736b0a20256f59570d74d7735e7
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-10-08 19:11:09 -0300

    Initial Commit

---
+++ BadgeExample/BadgeExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadgeExampleApp.swift
+//  BadgeExample
+//
+//  Created by Raphael Cerqueira on 08/10/20.
+//
+
+import SwiftUI
+
+@main
+struct BadgeExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
